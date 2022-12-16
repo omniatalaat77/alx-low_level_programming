c541ba4041cbd662859f82b93de06012a64b0695
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-16 08:59:03 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'n'; c<= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
