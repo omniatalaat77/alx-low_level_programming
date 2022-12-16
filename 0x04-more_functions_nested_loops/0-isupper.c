6d71ccb6006e3081fba91dce9530bb3cc6291015
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-16 13:57:28 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,11 +6,16 @@
  * Return: 1 for upper letter or 0 for any else
  */
 
-int _isupper(int x)
+int _isupper(int c)
 
 {
-	if (x >= 65 && x <= 90)
-	return (1);
+
+	if ((c >= 'A') && (c <= 'Z'))
+
+		return (1);
+
+
 
 	return (0);
+
 }
