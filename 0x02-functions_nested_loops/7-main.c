18b4269a00af5075396b5678f78cd4c5441d36eb
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-17 13:03:54 -0800

    7-last_digit

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * main - check the code
@@ -10,9 +11,7 @@ int main(void)
 {
 	int r;
 
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
+	r = print_last_digit(98);
 	_putchar('0' + r);
 	_putchar('\n');
 	return (0);
