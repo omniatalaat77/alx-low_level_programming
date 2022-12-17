e34c87c448c5baea9c30143d0ee6c5f11900a4e4
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-17 14:11:41 -0800

    11-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 #include <stdio.h>
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98
- * @n: The number to start printing from 
+ * @n: The number to start printing from
  * Return: Always 0.
  */
 
