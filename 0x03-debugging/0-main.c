ec7263d87274733b12620a8b763cb35d02a26128
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-17 15:28:51 -0800

    0x03-debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,7 +4,6 @@
  * main - testsfunction that prints if integer is positive or negative
  * Return: 0
  */
-
 int main(void)
 
 {
@@ -12,6 +11,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
