800ec127a031e8c58e0c242f2e9fcb731c678875
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-19 07:03:00 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,9 +9,7 @@
  * @year: year
  * Return: void
  */
-
-void print_remaning_days(int month, int day, int year)
-
+void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
 	{
