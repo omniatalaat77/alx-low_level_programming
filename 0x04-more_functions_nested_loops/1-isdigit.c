75a8c7dbd06db169f5c941dfbcc501cb36643828
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-19 07:31:56 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,6 @@
 #include "main.h"                                                                                                                                      
-
 /**
- * _isupper - Check if a character is a digit
+ * _isdigit - Check if a character is a digit
  * @x: The number to be checked
  * Return: 1 for a character that will be a digit or 0 for any else
  */
