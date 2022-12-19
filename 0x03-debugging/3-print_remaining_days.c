383ad559def7d6a0dfacd2f86196c99326ad5df1
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-19 06:14:24 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,7 @@
 void print_remaning_days(int month, int day, int year)
 
 {
-	if ((year % 4 == 0) && 400 == 0 || (year % 100 != 0))
+	if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
 	{
 	if (month >= 3 && day >= 60)
 	{
@@ -26,12 +26,12 @@ void print_remaning_days(int month, int day, int year)
 	{
 	if (month == 2 && day == 60)
 	{
-	printf("Invalid date: %02d/%02d/%02d", month, day - 31, year);
+	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
 	else
 	{
 	printf("Day of the year: %d\n", day);
-	printf("remaining days: %d\n", 365 - day);
+	printf("Remaining days: %d\n", 365 - day);
 	}
 	}
 }
