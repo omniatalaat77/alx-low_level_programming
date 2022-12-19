3501bf67ff2533d04b16e39a33b88768c6a58007
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-19 07:43:55 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,14 +4,13 @@
  * @x: The number to be checked
  * Return: 1 for a character that will be a digit or 0 for any else
  */
-
 int _isdigit(int x)
-
 {
 
 	if (x >= 48 && x <= 57)
 	{
 	return (1);
 	}
+
 	return (0);
 }
