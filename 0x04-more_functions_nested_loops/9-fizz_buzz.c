5ff51ddcbc38c220163ba26045c94b41e80409c7
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-19 11:39:02 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints a fizz Buzz program
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+	if ((x % 3 == 0) && (x % 5 == 0))
+	{
+	printf("fizzBuzz");
+	}
+	else if (x % 3 == 0)
+	{
+	printf("fizz");
+	}
+	else if (x % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%d", x);
+	}
+	if (x != 100)
+	{
+	printf(" ");
+	}
+	}
+	printf("\n");
+	return (0);
+}
