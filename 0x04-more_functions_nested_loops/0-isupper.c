ca648b66520592a2f4c1751ca92ab34275c7d603
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-19 07:24:23 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * isupper - Check if a letter is upper
+ * _isupper - Check if a letter is upper
  * @x: The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
+
 int _isupper(int x)
 {
 
