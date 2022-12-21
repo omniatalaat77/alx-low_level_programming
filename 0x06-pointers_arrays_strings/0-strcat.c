62b0924fea923568ff30cb17c013fc6fb64f2e39
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-21 13:59:22 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Find the end of the dest string
+ * Append src to end of dest
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+	char *dest_end = dest;
+	while (*dest_end != '\0')
+	{
+		dest_end++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest_end = *src;
+		dest_end++;
+		src++;
+	}
+	*dest_end = '\0';
+	return dest;
+}
