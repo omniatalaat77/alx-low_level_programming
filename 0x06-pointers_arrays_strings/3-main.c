e1bc72b0c564153c60be622ff71b20c99913885d
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-21 15:01:55 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -6,7 +6,9 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
+
 {
     char s1[] = "Hello";
     char s2[] = "World!";
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,6 +17,5 @@ int _strcmp(char *s1, char *s2)
 	s1++;
 	s2++;
 	}
-
 	return (*s1 - *s2);
 }
