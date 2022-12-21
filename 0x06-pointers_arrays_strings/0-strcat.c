8280c1e8c39655f94921e5f2a4d2e62cdf00a8ae
omniatalaat77 <talaatomnia77@gmail.com>
2022-12-21 14:07:41 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,17 +10,17 @@ char *_strcat(char *dest, char *src)
 
 {
 	char *dest_end = dest;
-	while (*dest_end != '\0')
-	{
-		dest_end++;
-	}
+	    while (*dest_end != '\0')
+	    {
+		    dest_end++;
+	    }
 
-	while (*src != '\0')
-	{
-		*dest_end = *src;
-		dest_end++;
-		src++;
-	}
-	*dest_end = '\0';
-	return dest;
+	    while (*src != '\0')
+	    {
+		    *dest_end = *src;
+		    dest_end++;
+		    src++;
+	    }
+	    *dest_end = '\0';
+	    return dest;
 }
