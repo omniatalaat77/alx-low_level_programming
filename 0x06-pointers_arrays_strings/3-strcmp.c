4ec02702780f25209abd4068d9aecab9f61797bd
omniatalaat77 <talaatomnia77@gmail.com>
2023-01-03 07:01:25 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - Compares pointers to two strings
  * @s1: A pointer to the first string to be compared
  * @s2: A pointer to the second string to be compared
- * Return: If str1 < str2, the negative difference of the first unmatched chars
+ * Return: If str1 < str2, the negative difference of first unmatched chars
  * If str1 == str2, 0.
  * If str1 > str2, the positive difference of the first unmatched characters
  *
