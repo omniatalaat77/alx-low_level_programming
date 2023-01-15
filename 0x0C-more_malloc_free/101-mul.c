b442c7cc23e4a54f87cb6f70e27f0e0f41f76f99
omniatalaat77 <talaatomnia77@gmail.com>
2023-01-15 12:29:31 -0800

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -39,7 +39,7 @@ int _strlen(char *s)
 }
 
 /**
- * errors - handels errors for main
+ * errors - handles errors for main
  */
 void errors(void)
 {
@@ -52,12 +52,12 @@ void errors(void)
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Returm: always 0 (Success)
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a =0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -74,16 +74,17 @@ int main(int argc, char *argv[])
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 +len2 + 1] = carry % 10;carry /= 10;
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0; i < len -1; i++)
+	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			a = 1;
